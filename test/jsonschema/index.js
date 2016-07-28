8bd192820b1566c53edc96ca730b0ee66ca1db0a
Petr Pchelko <ppchelko@wikimedia.org>
2016-07-28 15:51:12 -0700

    Test: Check that every event schema has correct meta object
    Change-Id: I61a7ac07f682d04b4c7817742fdbc483041ee095

--- test/jsonschema/index.js
+++ test/jsonschema/index.js
@@ -6,6 +6,56 @@ var assert = require('assert');
 var Ajv = require('ajv');
 var preq = require('preq');
 
+var metaSchema = yaml.safeLoad(fs.readFileSync(__dirname + '/meta_schema.yaml', 'utf8'));
+
+// Checks whether the schema contains each and every property
+// from the 'superschema'
+assert.isSuperSchema = function(schema, example, path) {
+    path = path || '';
+    if (typeof schema !== typeof example
+            || Array.isArray(schema) !== Array.isArray(example)) {
+        throw new assert.AssertionError({
+            message: 'Error at path: ' + path,
+            expected: example,
+            actual: schema || {}
+        });
+    }
+    if (Array.isArray(schema)) {
+        // For arrays we need to check that all of the elements from example
+        // are also in the schema
+        if (!example.every(function(element) {
+            return schema.indexOf(element) !== -1;
+        })) {
+            throw new assert.AssertionError({
+                message: 'Error at path: ' + path,
+                expected: example,
+                actual: schema || []
+            });
+
+        }
+    }
+    if (typeof example === 'object') {
+        // Go recursively
+        return Object.keys(example).forEach(function(key) {
+            if (key === 'title'
+                    || key === 'description') {
+                // These are for documentation mostly, so exclude them
+                return;
+            }
+            assert.isSuperSchema(schema[key], example[key], path + '.' + key);
+        });
+    }
+
+    if (schema !== example) {
+        throw new assert.AssertionError({
+            message: 'Error at path: ' + path,
+            expected: example,
+            actual: schema || []
+        });
+    }
+
+};
+
 describe('Json schema', function() {
     var ajv = new Ajv();
     var baseDir = __dirname + '/../../jsonschema/';
@@ -15,8 +65,9 @@ describe('Json schema', function() {
             if (fs.statSync(baseDir + filePath).isDirectory()) {
                 browseDir(filePath);
             } else {
+                var schema = yaml.safeLoad(fs.readFileSync(baseDir + filePath, 'utf8'));
+
                 it('Validity: ' + filePath, function() {
-                    var schema = yaml.safeLoad(fs.readFileSync(baseDir + filePath, 'utf8'));
                     assert.notEqual(schema, undefined);
                     assert.notEqual(schema['$schema'], undefined);
                     return preq.get({
@@ -31,6 +82,14 @@ describe('Json schema', function() {
                             });
                         }
                     });
+                });
+
+                if (filePath.indexOf('/test/event/1.yaml') !== -1) {
+                    return;
+                }
+
+                it('Contains meta: ' + filePath, function() {
+                    assert.isSuperSchema(schema, metaSchema);
                 });
             }
         });
