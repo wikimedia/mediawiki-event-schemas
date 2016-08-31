e915290ca9ef97c1ec94114c17562eee225539ef
Petr Pchelko <ppchelko@wikimedia.org>
2016-08-31 12:51:06 -0700

    Tests: check that all the schemas contain common properties
    We've introduced a notion of page/revision/user common properties.
    These set of tests check that all the common properties are present
    in individual events, and that the same set of properties is required.
    
    The duplocation of 'mediawiki_common_props' file will be removed
    after we delete old schemas.
    
    Change-Id: I2d889f835cbf002bfc7020e47475fd5eb81b8b93

--- test/jsonschema/index.js
+++ test/jsonschema/index.js
@@ -1,98 +1,97 @@
 "use strict";
 
-var fs = require('fs');
-var yaml = require('js-yaml');
-var assert = require('assert');
-var Ajv = require('ajv');
-var preq = require('preq');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const file = require('file');
+const path = require('path');
 
-var metaSchema = yaml.safeLoad(fs.readFileSync(__dirname + '/meta_schema.yaml', 'utf8'));
+const assert = require('assert');
+const Ajv = require('ajv');
 
-// Checks whether the schema contains each and every property
-// from the 'superschema'
-assert.isSuperSchema = function(schema, example, path) {
+const ajv = new Ajv({});
+const baseDir = path.join(__dirname, '/../../jsonschema/');
+
+// Checks whether the schema contains each and every property from the 'superschema'
+assert.isSuperSchema = (schema, example, path) => {
     path = path || '';
-    if (typeof schema !== typeof example
-            || Array.isArray(schema) !== Array.isArray(example)) {
+    if (typeof schema !== typeof example || Array.isArray(schema) !== Array.isArray(example)) {
         throw new assert.AssertionError({
-            message: 'Error at path: ' + path,
+            message: `Error at path: ${path}`,
             expected: example,
             actual: schema || {}
         });
-    }
-    if (Array.isArray(schema)) {
+    } else if (Array.isArray(schema)) {
         // For arrays we need to check that all of the elements from example
         // are also in the schema
-        if (!example.every(function(element) {
-            return schema.indexOf(element) !== -1;
-        })) {
+        if (!example.every((element) => schema.indexOf(element) !== -1)) {
             throw new assert.AssertionError({
-                message: 'Error at path: ' + path,
+                message: `Error at path: ${path}`,
                 expected: example,
                 actual: schema || []
             });
 
         }
-    }
-    if (typeof example === 'object') {
+    } else if (typeof example === 'object') {
         // Go recursively
-        return Object.keys(example).forEach(function(key) {
-            if (key === 'title'
-                    || key === 'description') {
-                // These are for documentation mostly, so exclude them
-                return;
-            }
-            assert.isSuperSchema(schema[key], example[key], path + '.' + key);
-        });
-    }
-
-    if (schema !== example) {
+        return Object.keys(example)
+        .filter((key) => key !== 'title' && key !== 'description')
+        .forEach((key) => assert.isSuperSchema(schema[key], example[key], path + '.' + key));
+    } else if (schema !== example) {
         throw new assert.AssertionError({
-            message: 'Error at path: ' + path,
+            message: `Error at path: ${path}`,
             expected: example,
-            actual: schema || []
+            actual: schema
         });
     }
-
 };
 
-describe('Json schema', function() {
-    var ajv = new Ajv();
-    var baseDir = __dirname + '/../../jsonschema/';
-    function browseDir(pathFromBase) {
-        fs.readdirSync(baseDir + pathFromBase).forEach(function(fileName) {
-            var filePath = pathFromBase + '/' + fileName;
-            if (fs.statSync(baseDir + filePath).isDirectory()) {
-                browseDir(filePath);
-            }
-            else if (filePath.endsWith('.yaml') || filePath.endsWith('.json')) {
-                var schema = yaml.safeLoad(fs.readFileSync(baseDir + filePath, 'utf8'));
-                it('Validity: ' + filePath, function() {
-                    assert.notEqual(schema, undefined);
-                    assert.notEqual(schema['$schema'], undefined);
-                    return preq.get({
-                        uri: schema['$schema']
-                    })
-                    .then(function(res) {
-                        var validate = ajv.compile(res.body);
-                        var valid = validate(schema);
-                        if (!valid) {
-                            throw new assert.AssertionError({
-                                message: ajv.errorsText(valid.errors)
-                            });
-                        }
-                    });
-                });
+const isYamlJson = (fileName) => {
+    return fileName.endsWith('.yaml') || fileName.endsWith('.json');
+};
 
-                if (filePath.indexOf('/test/event/1.yaml') !== -1) {
+const loadYaml = (path) => {
+    return yaml.safeLoad(fs.readFileSync(path), 'utf8');
+};
+
+const cropExtension = (fileName) => {
+    return fileName.replace(/\.[^.]+$/, '');
+};
+
+/**
+ * Here we generate a set of tests that check individual schemas for compliance
+ * with common properties schemas, e.g. thay contain all the same properties,
+ * the types are correct and required isn't missing.
+ *
+ * We've set up a directory structure in the test folder mapping the directory
+ * structure of events. As we transfer this directory structure, we find common
+ * schemas along the path and test that all the schemas in subdirectories comply
+ * to this common schema.
+ */
+describe('Json schema', () => {
+    file.walkSync(baseDir, (dirPath, dirs, files) => {
+        files.filter(isYamlJson).forEach((fileName) => {
+            const schemaPath = path.join(path.relative(baseDir, dirPath), fileName);
+            const schema = loadYaml(path.join(dirPath, fileName));
+            describe('Schema ' + cropExtension(schemaPath), () => {
+                it('Must be valid JSON-Schema', () => ajv.validateSchema(schema, true));
+
+                if (schemaPath === 'test/event/1.yaml') {
                     return;
                 }
 
-                it('Contains meta: ' + filePath, function() {
-                    assert.isSuperSchema(schema, metaSchema);
-                });
-            }
+                var pathSegments = schemaPath.split('/');
+                for (var idx = 0; idx < pathSegments.length - 1; idx++) {
+                    var subPath = path.join(__dirname, pathSegments.slice(0, idx).join('/'));
+                    if (fs.existsSync(subPath) && fs.statSync(subPath).isDirectory()) {
+                        fs.readdirSync(subPath).filter(isYamlJson).forEach((fileName) => {
+                            const exampleSchema = loadYaml(path.join(subPath, fileName));
+                            it('Must contain ' + cropExtension(fileName), () => {
+                                assert.isSuperSchema(schema, exampleSchema)
+                            });
+                        });
+                    }
+                }
+            });
         });
-    }
-    browseDir('');
+    });
 });
